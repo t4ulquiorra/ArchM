befc2a041708e2d8da3fd26bca722e156dff5f84
t4ulquiorra <t4.ulquiorra@gmail.com>
2026-09-06 03:16:09 +0530

    fix: import remember in AppBar.kt

--- app/src/main/kotlin/com/archm/player/ui/utils/AppBar.kt
+++ app/src/main/kotlin/com/archm/player/ui/utils/AppBar.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.material3.TopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
 import androidx.compose.ui.input.nestedscroll.NestedScrollSource
