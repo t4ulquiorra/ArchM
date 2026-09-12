db7041dd0e5fea81498221c000037eea07af6b8c
t4ulquiorra <t4.ulquiorra@gmail.com>
2026-09-12 11:11:05 +0530

    fix(ui): import wrapContentHeight in Items.kt

--- app/src/main/kotlin/com/archm/player/ui/component/Items.kt
+++ app/src/main/kotlin/com/archm/player/ui/component/Items.kt
@@ -45,6 +45,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
