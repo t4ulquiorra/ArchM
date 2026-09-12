92e1548f63b1bf2281d4db44808e8b39f931eec6
t4ulquiorra <t4.ulquiorra@gmail.com>
2026-09-12 06:33:54 +0530

    fix(ui): cleanly import fillMaxHeight in Items and ArtistScreen

--- app/src/main/kotlin/com/archm/player/ui/screens/artist/ArtistScreen.kt
+++ app/src/main/kotlin/com/archm/player/ui/screens/artist/ArtistScreen.kt
@@ -50,6 +50,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
