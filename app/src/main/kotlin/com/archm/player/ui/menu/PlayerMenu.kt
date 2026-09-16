84bcd9cbd5954d792cd32032b4cecfc3641de86d
t4ulquiorra <t4.ulquiorra@gmail.com>
2026-09-16 22:22:11 +0530

    fix(build): add @OptIn(ExperimentalMaterial3Api::class) to PlayerMenu

--- app/src/main/kotlin/com/archm/player/ui/menu/PlayerMenu.kt
+++ app/src/main/kotlin/com/archm/player/ui/menu/PlayerMenu.kt
@@ -30,6 +30,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.HorizontalDivider
@@ -102,6 +103,7 @@ import kotlin.math.log2
 import kotlin.math.pow
 import kotlin.math.round
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PlayerMenu(
     mediaMetadata: MediaMetadata?,
